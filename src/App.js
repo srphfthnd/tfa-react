bea039cf1206f090bbe90cac1aeb772d53027f3a
John Mark Sena <30517958+srphfthnd@users.noreply.github.com>
2024-12-15 22:26:06 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { NavigationBar } from './components/header/navigation-bar/navigation-bar';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar/>
+      <NavigationBar/>
+      <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function NavigationBar() {
+    return (
+        <div className='top-bar'>
+            <div>
+                Hey
+            </div>
+        </div>
+    )
+};
