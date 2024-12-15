988e93b30f3898a726e4e12baddc2fdf55af70d1
JnKnnth <kenn.abejero@gmail.com>
2024-12-16 03:16:04 +0800

    added body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { NavigationBar } from './components/header/navigation-bar/navigation-bar';
+import { Container } from './components/header/container/container';
 
 function App() {
   return (
     <div>
       <NavigationBar/>
+      <Container/>
     </div>
   );
 }
 
 export default App;
-//sdsdsdsdsd
---
+++ src/components/header/container/container.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./container.scss";
+
+export function Container() {
+    return (
+      <div class='background'>
+          <div className='center-text'>
+              <h1>New Company Name</h1>
+              <div className='about-us'>
+               <h1>Contact Us</h1>
+               <h1>About Us</h1>
+                 </div>
+          </div>
+      </div> 
+     /*div class="container">
+  <div class="item">Item 1</div>
+  <div class="item">Item 2</div>
+  <div class="item">Item 3</div>
+</div>*/
+    )
+};
